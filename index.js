860465c2fe5eb29f298d2ec066f5cf74882f0dfa
板谷優里 <itayayuri@yuri-mac.local>
2021-07-28 14:32:38 +0900

    [Stadied]2021/07/28文字列とユニコード/Code Point

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const color = [
-    { color: "red" },
-    { color: "green" },
-    { color: "blue" },
-];
-
-const blueColor = color.find((obj) => {
-    return obj.color === "blue";
-});
-
-console.log(blueColor);
-
-const whiteColor = color.find((obj) => {
-    return obj.color === "white";
-});
-console.log(whiteColor);
+const codePointOfい = "い".codePointAt(0);
+// 12354の16進数表現は"3042"
+const hexOfい = codePointOfい.toString(16);
+console.log(hexOfい);// => "3042"
+// Unicodeエスケープで"あ"を表現できる
+console.log("\u{3044}"); // => "あ"
