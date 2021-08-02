26b17c69568e134e41e3a1085502afac791db079
yuri0427a <yuri0427ita@gmail.com>
2021-08-03 01:14:54 +0900

    [Staried]2021/08/03

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const codePointOfい = "い".codePointAt(0);
-// 12354の16進数表現は"3042"
-const hexOfい = codePointOfい.toString(16);
-console.log(hexOfい);// => "3042"
-// Unicodeエスケープで"あ"を表現できる
-console.log("\u{3044}"); // => "あ"
+const createCounter = () => {
+    let count = 0;
+        // `increment`関数は`createCounter`関数のスコープに定義された`変数`count`を参照している
+        count = count + 1;
+        return count;
+};
+
+const myCounter = createCounter();
+console.log(myCounter);
+console.log(myCounter); 
