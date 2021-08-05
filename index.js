a2ebd1cfe93e58a4087c21f291c9ba54634fec6b
yuri0427a <yuri0427ita@gmail.com>
2021-08-05 23:57:25 +0900

    [Stadied]2021/08/05

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const createCounter = () => {
-    let count = 0;
-        // `increment`関数は`createCounter`関数のスコープに定義された`変数`count`を参照している
-        count = count + 1;
-        return count;
+const callCallback = (callback) => {
+    // `callback`を呼び出す実装
 };
-
-const myCounter = createCounter();
-console.log(myCounter);
-console.log(myCounter); 
