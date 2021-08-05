8aad2fdd9eacb2f0ac3809a68906b8a17fed1c36
yuri0427a <yuri0427ita@gmail.com>
2021-08-06 01:19:45 +0900

    [Stadied]2021/08/06

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const callCallback = (callback) => {
-    // `callback`を呼び出す実装
-};
+class Counter{
+    constructor(){
+        this.count = 3;
+    }
+
+    increment() {
+        this.count++;
+    }
+}
+
+const counterA = new Counter();
+const counterB = new Counter();
+
+counterA.increment();
+
+console.log(counterA.increment === counterB.increment);
