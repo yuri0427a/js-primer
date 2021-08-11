f882146b4d28b719d6b1b1a98ee0c2a1fafd961f
yuri0427a <yuri0427ita@gmail.com>
2021-08-12 02:33:33 +0900

    [Stadied]2021/08/12

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-class Counter{
-    constructor(){
-        this.count = 3;
-    }
-
-    increment() {
-        this.count++;
-    }
+try {
+    console.log("try節:この行は実行されます");
+    undefinedFunction();
+}catch (error) {
+    // 例外が発生したあとはこのブロックが実行される
+    console.log("catch節:この行は実行されます");
+    console.log(error instanceof ReferenceError); // => true
+    console.log(error.message); // => "undefinedFunction is not defined"
+} finally {
+    // このブロックは例外の発生に関係なく必ず実行される
+    console.log("finally節:この行は実行されます");
 }
-
-const counterA = new Counter();
-const counterB = new Counter();
-
-counterA.increment();
-
-console.log(counterA.increment === counterB.increment);
