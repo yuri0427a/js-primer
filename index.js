3a0455de1065eb91c87cfc370934465df117c00a
yuri0427a <yuri0427ita@gmail.com>
2021-08-13 12:47:04 +0900

    [Stadied]2021/08/13

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-try {
-    console.log("try節:この行は実行されます");
-    undefinedFunction();
-}catch (error) {
-    // 例外が発生したあとはこのブロックが実行される
-    console.log("catch節:この行は実行されます");
-    console.log(error instanceof ReferenceError); // => true
-    console.log(error.message); // => "undefinedFunction is not defined"
-} finally {
-    // このブロックは例外の発生に関係なく必ず実行される
-    console.log("finally節:この行は実行されます");
+function block(timeout) {
+    const startTime = Date.now();
+
+    while (true) {
+        const startTime = Date.now() - startTime;
+        if (diffTime >= timeout) {
+            return; // 指定時間経過したら関数の実行を終了
+        }
+    }
 }
+
+console.log("処理を開始");
+blockTime(1000); // 他の処理を1000ミリ秒（1秒間）ブロックする
+console.log("この行が呼ばれるまで処理が1秒間ブロックされる");
