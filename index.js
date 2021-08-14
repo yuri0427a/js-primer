8577676c203bce036255871c535da2fe3b0fdbb9
yuri0427a <yuri0427ita@gmail.com>
2021-08-15 01:28:41 +0900

    [Stadied]2021/08/15

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-function dummyFetch(path, callback) {
-    setTimeout(() => {
-        if (path.startsWith("/success")) {
-            callback(null, { body: `Response body of ${path}` });
-        } else {
-            callback(new Error("NOT FOUND"));
-        }
-    }, 1000 * Math.random());
+function dummyFetch(path) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (path.startsWith("/success")) {
+                resolve({ body: `Response body of ${path}` });
+            } else {
+                reject(new Error("NOT FOUND"));
+            }
+        }, 1000 * Math.random())
+    })
 }
 
-dummyFetch("/success/data", (error, response) => {
-    if (error) {
-        // この行は実行されません
-    } else {
-        console.log(response); // => { body: "Response body of /success/data" }
-    }
+dummyFetch("/success/data").then(function onFulfilled(response) {
+    console.log(response);
+}, function onRejected(error) {
+    // この行は実行されません
 });
-// /failure/data にリソースは存在しないので、`error`にはエラーオブジェクトが入る
-dummyFetch("/failure/data", (error, response) => {
-    if (error) {
-        console.log(error.message); // => "NOT FOUND"
-    } else {
-        // この行は実行されません
-    }
-})
+
+dummyFetch("/failure/data").then(function onFulfilled(response){
+
+}, function onRejcted(error) {
+    console.log(error);
+});
