d2c81a8d1a44a55fd39b339b4c323c300a83e700
yuri0427a <yuri0427ita@gmail.com>
2021-08-19 00:55:22 +0900

    [Starded]2021/08/19

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-Promise.reject(new Error("エラー")).catch(error => {
-    console.log(error); // Error: エラー
-}).then(() => {
-    console.log("thenのコールバック関数が呼び出される");
-});
+function delay(timeoutMs){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(timeoutMs);
+        }, timeoutMs);
+    });
+}
+
+const promise1 = delay(1);
+const promise2 = delay(2);
+const promise3 = delay(3);
+
+Promise.all([promise1, promise2, promise3]).then(function (values) {
+    console.log(values);
+})
